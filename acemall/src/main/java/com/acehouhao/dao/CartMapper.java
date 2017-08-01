a6555e2d8f1de5cd724c3b394b3b893116b2ff76
Hao HOU <houhao118@163.com>
2017-08-01 17:46:23 +0800

    项目初始化

---
+++ acemall/src/main/java/com/acehouhao/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.acehouhao.dao;
+
+import com.acehouhao.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ acemall/src/main/java/com/acehouhao/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.acehouhao.dao;
+
+import com.acehouhao.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
