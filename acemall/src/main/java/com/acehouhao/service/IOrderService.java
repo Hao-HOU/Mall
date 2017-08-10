2375b812b301c373fb052f59131ce480ac48cff1
Hao HOU <houhao118@163.com>
2017-08-10 20:02:11 +0800

    订单模块

--- acemall/src/main/java/com/acehouhao/service/IOrderService.java
+++ acemall/src/main/java/com/acehouhao/service/IOrderService.java
@@ -1,6 +1,8 @@
 package com.acehouhao.service;
 
 import com.acehouhao.common.ServerResponse;
+import com.acehouhao.vo.OrderVo;
+import com.github.pagehelper.PageInfo;
 
 import java.util.Map;
 
@@ -13,4 +15,24 @@ public interface IOrderService {
     ServerResponse aliCallback(Map<String, String> params);
 
     ServerResponse queryOrderPayStstus(Integer userId, Long orderNo);
+
+    ServerResponse createOrder(Integer userId, Integer shippingId);
+
+    ServerResponse<String> cancel(Integer userId, Long orderNo);
+
+    ServerResponse getOrderCartProduct(Integer userId);
+
+    ServerResponse<OrderVo> getOrderDetail(Integer userId, Long orderNo);
+
+    ServerResponse<PageInfo> getOrderList(Integer userId, int pageNum, int pageSize);
+
+    //backend
+
+    ServerResponse<PageInfo> manageList(int pageNum, int pageSize);
+
+    ServerResponse<OrderVo> manageDetail(Long orderNo);
+
+    ServerResponse<PageInfo> manageSearch(Long orderNo, int pageNum, int pageSize);
+
+    ServerResponse<String> manageSendGoods(Long orderNo);
 }
